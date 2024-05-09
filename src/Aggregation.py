2cf36f705dc6a2e4b3fc41d41b60062648d60edd
Hepecho <echoeswyq@outlook.com>
2024-05-09 13:46:49 +0800

    fix problem in KrumAvg

--- src/Aggregation.py
+++ src/Aggregation.py
@@ -5,6 +5,12 @@ from torch.linalg import vector_norm
 
 
 def FedAvg(clients, server, server_lr):
+    """
+    :param clients: 工作节点回传的梯度集合，有序字典构成的list
+    :param server: 参数服务器的当前参数，有序字典
+    :param server_lr: 当前server的学习率
+    :return: 平均聚合后的参数，有序字典
+    """
     g_avg = copy.deepcopy(clients[0])
     for k in g_avg.keys():
         for i in range(1, len(clients)):
@@ -15,19 +21,23 @@ def FedAvg(clients, server, server_lr):
 
 
 def KrumAvg(clients, server, server_lr, max_num, topk=1):
+    """
+    :param clients: 工作节点回传的梯度集合，有序字典构成的list
+    :param server: 参数服务器的当前参数，有序字典
+    :param server_lr: 当前server的学习率
+    :param max_num: 由n-b-2得到，是krum考虑的最接近梯度向量的数量
+    :param topk: muti-krum的插值参数，当topk=1时即为krum，topk=n时即为FedAvg
+    :return: Krum聚合后的参数，有序字典
+    """
     num_workers = len(clients)
     g_avg = copy.deepcopy(clients[0])
     scores = []
-    # print(g_avg.keys())
     dist_ij = torch.zeros(num_workers, num_workers).to(g_avg['conv1.0.weight'].device)
     for i in range(num_workers):
-        for j in range(num_workers):
-            if j == i:
-                continue
-            if j < i:
-                dist_ij[i][j] = dist_ij[j][i]
+        for j in range(i + 1, num_workers):  # dist_ij[i][j] == dist[j][i]
             for k in g_avg.keys():
                 dist_ij[i][j] += vector_norm(clients[i][k] - clients[j][k], ord=2)
+            dist_ij[j][i] = dist_ij[i][j]
         dist_ij[i].sort()
         scores.append(sum(dist_ij[i][1:max_num + 1]))  # 忽略i==j
 
